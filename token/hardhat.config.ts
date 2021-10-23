7f37e55fb7787edeaa26f1a8c55c1a278c50d4ab
Francisco Sousa <francisco.sousa@hey.com>
2021-10-24 00:05:21 +0100

    Fix

--- token/hardhat.config.ts
+++ token/hardhat.config.ts
@@ -77,8 +77,8 @@ const config: HardhatUserConfig = {
       blockGasLimit: 10000000,
       initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
     },
-    rinkeby: createNetworkConfig("rinkeby"),
-    ropsten: createNetworkConfig("ropsten"),
+    rinkeby: ETH_PRIVATE_KEY ? createNetworkConfig("rinkeby") : undefined,
+    ropsten: ETH_PRIVATE_KEY ? createNetworkConfig("ropsten") : undefined,
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
